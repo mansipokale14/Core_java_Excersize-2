961a0bb10fef7790ae72d6e0e8519b105289231f
Mansi Pokale <mansipokale1405@gmail.com>
2024-11-27 17:34:59 +0530

    Add files via upload

---
+++ Excersice_2/Q5.java
@@ -0,0 +1,27 @@
+package Excersice_2;
+
+import java.util.Scanner;
+
+public class Q5 {
+
+	public static void main(String[] args) {
+		//5. WAP to print area of a rectangle (A=width*height)?
+		
+				int height,width,result;
+				//scanner class used for user input
+				Scanner sc= new Scanner(System.in);
+				
+				System.out.println("Enter the height : ");
+				height=sc.nextInt();
+				
+				System.out.println("Enter the width : ");
+				width=sc.nextInt();
+				
+				result=height*width;
+				
+				System.out.println("Area of rectangle is : "+result);
+
+
+	}
+
+}
